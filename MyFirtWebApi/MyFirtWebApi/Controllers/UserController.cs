9016e82145094b8fcb2521f949dcc65fc761d9c8
Edwin Yarro <>
2020-02-08 18:15:53 -0500

    add user model

--- MyFirtWebApi/MyFirtWebApi/Controllers/UserController.cs
+++ MyFirtWebApi/MyFirtWebApi/Controllers/UserController.cs
@@ -1,11 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Data.SqlClient;
 using Microsoft.Extensions.Logging;
 using MyFirtWebApi.Context;
+using MyFirtWebApi.Models;
 
 namespace MyFirtWebApi.Controllers
 {
@@ -13,35 +13,78 @@ namespace MyFirtWebApi.Controllers
     [Route("[controller]")]
     public class UserController : ControllerBase
     {
-        string connectionString = "Data Source=192.168.250.65;Initial Catalog=pingflood;User ID=sa;Password=shawi123@";
+        /// <summary>
+        /// _connectionString
+        /// </summary>
+        private readonly string _connectionString = "Data Source=192.168.250.65;Initial Catalog=pingflood;User ID=sa;Password=shawi123@";
 
+        /// <summary>
+        /// _logger
+        /// </summary>
         private readonly ILogger<UserController> _logger;
 
+        //UserController
         public UserController(ILogger<UserController> logger)
         {
-            _logger = logger;
+            this._logger = logger;
         }
 
+        /// <summary>
+        /// Get
+        /// </summary>
+        /// <param name="userId"></param>
+        /// <returns></returns>
         [HttpGet]
-        public IEnumerable<User> Get()
+        public IEnumerable<User> Get(int userId)
         {
-            SqlConnection cnn = new SqlConnection(connectionString);
-
-            List<User> users;
-
             try
             {
+                SqlConnection cnn = new SqlConnection(_connectionString);
+
                 cnn.Open();
 
-                using (var db = new UserContext())
+                List<User> users;
+
+                using (UserContext context = new UserContext())
                 {
-                    users = db.Users
+                    users = context.Users
                        .ToList();
                 }
 
                 cnn.Close();
 
                 return users;
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+        }
+
+        /// <summary>
+        /// Create
+        /// </summary>
+        /// <param name="user"></param>
+        /// <returns></returns>
+        [HttpPost]
+        public User Create(User user)
+        {
+            SqlConnection cnn = new SqlConnection(_connectionString);
+
+            try
+            {
+                cnn.Open();
+
+                using (UserContext context = new UserContext())
+                {
+                    Microsoft.EntityFrameworkCore.ChangeTracking.EntityEntry<User> u = context.Users.Add(user);
+                    //u.
+                    //var result = this.Get(u)
+                }
+
+                cnn.Close();
+
+                return null;
             }
             catch (Exception ex)
             {
---
+++ MyFirtWebApi/MyFirtWebApi/Models/User.cs
@@ -0,0 +1,24 @@
+﻿using System;
+namespace MyFirtWebApi.Models
+{
+    /// <summary>
+    /// User
+    /// </summary>
+    public class User
+    {
+        /// <summary>
+        /// Id
+        /// </summary>
+        public int Id { get; set; }
+
+        /// <summary>
+        /// Name
+        /// </summary>
+        public string Firstname { get; set; }
+
+        /// <summary>
+        /// Lastname
+        /// </summary>
+        public string Lastname { get; set; }
+    }
+}
